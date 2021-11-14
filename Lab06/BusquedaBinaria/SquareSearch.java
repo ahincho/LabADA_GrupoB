45c5f99adc12b35ee3ef64e811ca85e24230e41e
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2021-11-13 23:47:17 -0500

    Verificar si un numero es cuadrado basado en Busqueda Binaria

--- Lab06/BusquedaBinaria/SquareSearch.java
+++ Lab06/BusquedaBinaria/SquareSearch.java
@@ -12,94 +12,29 @@ public class SquareSearch {
 	
 	public static void main(String[] args) {
 		
-		// Suponiendo que se ingresa un arreglo con numeros enteros
-		// positivos, entonces se procede a buscar aquellos que son
-		// numeros cuadrados o de la forma (indice*indice)
+		// Metodo main del programa
 		
-		int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-		System.out.println(squareSearch(a, 7) + "\n");
-		System.out.println(squareSearch(a, 16) + "\n");
-		System.out.println(squareSearch(a, 49) + "\n");
-		System.out.println(squareSearch(a, 4) + "\n");
-		System.out.println(squareSearch(a, 11) + "\n");
-		
-	}
-	
-	public static int squareSearch(int[] a, int number) {
-		
-		// Usamos el metodo binarySearch() para ver si el numero
-		// existe o esta incluido dentro del arreglo
-		
-		int search = binarySearch(a, number);
-		
-		// Se comprobrara si es cuadrado perfecto, mediante las
-		// sumas consecutivas de los numeros impares
-		
-		// Formula
-		// n^2 = sumatoria desde 0 a 'n' de los (2n + 1)
-		
-		int sum = 0;
-		int nValue = 0;
-		
-		if(search == -1) {
-			
-			// Si no se encuentra dentro del arreglo devolver
-			// el valor de -1 (No se encontro en el arreglo)
-			
-			System.out.println("No se encontro " + number);
-			return search;
-			
-		} else {
-			
-			while(sum <= search) {
-				
-				if(sum == search) {
-					
-					// Si el numero es igual a una de las sumas
-					// devolver el numero y romper el ciclo
-					
-					System.out.println("El numero "
-							+ number + " es cuadrado");
-					break;
-					
-				} else {
-					
-					nValue++;
-					sum = sum + (2 * nValue - 1);
-					
-				}
-				
-			}
-			
-			if(sum > search) {
-				
-				// Si la suma es mayor al numero buscado entonces
-				// devolver -2 (Se encuentra en el array pero no
-				// es un numero cuadrado) para indicarlo.
-				
-				System.out.println("El numero "
-						+ number + " no es cuadrado");
-				search = -2;
-				
-			}
-			
-		}
-		
-		return search;
+		giveAnAnswer(squareSearch(7));
+		giveAnAnswer(squareSearch(16));
+		giveAnAnswer(squareSearch(49));
+		giveAnAnswer(squareSearch(4));
+		giveAnAnswer(squareSearch(11));
+		giveAnAnswer(squareSearch(13));
+		giveAnAnswer(squareSearch(6));
+		giveAnAnswer(squareSearch(9));
 		
 	}
 	
-	public static int binarySearch(int[] a, int search) {
+	public static int squareSearch(int number) {
 		
-		// Metodo Binary Search, recibe un arreglo estandar
-		// de numeros enteros y el numero entero que se desea
-		// buscar dentro del arreglo.
-		// Devuelve el numero si es que se encuentra dentro del
-		// arreglo y devuelve el valor de -1 si no se encuentra.
+		// Metodo SquareSearch recibe un numero entero
+		// y verifica si este es un numero cuadrado
 		
 		int left = 0;
-		int right = a.length - 1;
+		int right = number;
 		int mid;
+		
+		System.out.print("El numero " + number + " es cuadrado?: ");
 		
 		while(left <= right) {
 			
@@ -107,25 +42,24 @@ public class SquareSearch {
 			
 			mid = left + (right - left)/2;			
 			
-			if(a[mid] == search) {
+			if(mid * mid == number) {
 				
-				// Si el valor contenido en el indice del 'medio'
-				// es igual al que se esta buscando, se encontro.
+				// Si al multiplicar el valor de 'mid*mid' es
+				// igual a 'number' entonces si es un cuadrado
 				
-				System.out.println("Se encontro el numero " + search);
-				return a[mid];
+				return number;
 				
-			} else if (a[mid] < search) {
+			} else if (mid * mid < number) {
 				
-				// Si el valor del indice 'medio' es menor
-				// entonces buscar en la mitad superior
+				// Si el valor de 'mid*mid' es menor a 'number'
+				// pasaremos a buscar en la parte superior
 				
 				left = mid + 1;
 				
 			} else {
 				
-				// Si el valor del indice 'medio' es mayor
-				// entonces buscar en la mitad inferior
+				// Si el valor de 'mid*mid' es mayor a 'number'
+				// pasaremos a buscar en la parte inferior
 				
 				right = mid - 1;
 				
@@ -133,12 +67,27 @@ public class SquareSearch {
 			
 		}
 		
-		// Si no se encuentra el numero que se esta buscando
-		// entonces devolver un mensaje de 'no encontrado' y -1
-		
-		System.out.println("No se encontro el numero " + search);
+		// En este caso, el numero que se envio como
+		// parametro al metodo no es un numero cuadrado
 		
 		return -1;
+		
+	}
+	
+	public static void giveAnAnswer(int answer) {
+		
+		// Metodo auxiliar para mostrar una respuesta segun
+		// el dato devuelto por el metodo 'SquareSearch'
+		
+		if(answer != -1) {
+			
+			System.out.println("Es cuadrado!");
+			
+		} else {
+			
+			System.out.println("No es cuadrado!");
+			
+		}
 		
 	}
 	
